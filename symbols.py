1b52569e6d985023827eb248222e778cd88be73f
KER AND HAN <shalcozbi@gmail.com>
2025-06-12 17:03:11 +0800

    this part uses encapsulation since datas are grouped in a class

---
+++ symbols.py
@@ -0,0 +1,14 @@
+class SymbolSet:
+    def __init__(self):
+        self.symbol_counts = {
+            "💀": 2,
+            "🔥": 4,
+            "💎": 6,
+            "💰": 8
+        }
+        self.symbol_values = {
+            "💀": 5,
+            "🔥": 4,
+            "💎": 3,
+            "💰": 2
+        }
