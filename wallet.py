3b44660f88b3643488fdefbec1c66d044c2008bb
KER AND HAN <shalcozbi@gmail.com>
2025-06-22 13:11:23 +0800

    the def part uses abstraction since it gives a simple logic to check if the balance is enough

--- wallet.py
+++ wallet.py
@@ -8,3 +8,8 @@ class Wallet:
     def withdraw(self, amount):
         if self.can_afford(amount):
             self.balance -= amount
+        else:
+            raise ValueError("Not enough funds")
+
+    def can_afford(self, amount):
+        return self.balance >= amount
