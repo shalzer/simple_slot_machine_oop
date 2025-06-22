ada4b2c9ebe000f51236398337c53d871cc567d4
KER AND HAN <shalcozbi@gmail.com>
2025-06-22 13:51:49 +0800

    not an oop pillar

--- slot_machine.py
+++ slot_machine.py
@@ -8,3 +8,16 @@ class SlotMachine:
         self.cols = 3
 
     def get_spin_result(self):
+        all_symbols = self.symbols.get_symbol_pool()
+        columns = []
+
+        for _ in range(self.cols):
+            current_symbols = all_symbols[:]
+            column = []
+
+            for _ in range(self.rows):
+                value = random.choice(current_symbols)
+                current_symbols.remove(value)
+                column.append(value)
+
+            columns.append(column)
